bcd0ef099f21794b72cfd1f03b3cc5d2cb68d3c8
Traderyashhh <traderyashh01@gmail.com>
2025-07-25 21:52:22 +0530

    new index

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -116,11 +116,11 @@ const PlaceOrder = () => {
                     <div className='flex gap-3 flex-col lg:flex-row'>
                         <div onClick={() => setMethod('cashfree')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cashfree' ? 'bg-green-400' : ''}`}></p>
-                            <img className='h-5 mx-4' src="/upi.avif" alt="Cashfree" />
+                            <img className='h-5 mx-4' src={assets.cashfree_logo || assets.stripe_logo} alt="Cashfree" />
                         </div>
                         <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
-                            {/* <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p> */}
+                            <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
                         </div>
                     </div>
 
